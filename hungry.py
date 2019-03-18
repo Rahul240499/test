b037dcadfc6d64f2f7c3ca0218e746a20cedce17
RAHUL <rj240499@gmail.com>
2019-03-19 00:51:26 +0530

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -5,4 +5,10 @@ Created on Mon Mar 18 23:57:12 2019
 @author: rahulkumar
 """
 
-print("I'm hungry")
+# print("I'm hungry")
+hungry=input("are you hungry?")
+if hungry=="yes":
+    print("eat samosa")
+else:
+    print("do your homework")
+
